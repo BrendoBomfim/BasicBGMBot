a71d01b5a961d6bd33612658680414f6beabd293
Brendo Bomfim <brendobomfim@sciosolutions.com.br>
2018-09-28 14:05:34 -0300

    fix 1

--- pymessenger/bot.py
+++ pymessenger/bot.py
@@ -64,29 +64,30 @@ class Bot:
         Output:
             Response from API as <dict>
         """
-        print(notification_type, os.path.basename(attachment_path))
-        
+        print(type(recipient_id), type(attachment_type), type(attachment_path))
         payload = {
-            
+            'recipient': {
+                
+                    'id': recipient_id
+                
+            },
             'notification_type': notification_type,
             'message': {
-                {
+                
                     'attachment': {
                         'type': attachment_type,
                         'payload': {}
                     }
-                }
+                
             },
             'filedata': (os.path.basename(attachment_path), open(attachment_path, 'rb'))
         }
-        """
         multipart_data = MultipartEncoder(payload)
         multipart_header = {
             'Content-Type': multipart_data.content_type
         }
         return requests.post(self.graph_url, data=multipart_data,
-                             params=self.auth_args, headers=multipart_header).json() """
-        return ''
+                             params=self.auth_args, headers=multipart_header).json()
 
     def send_attachment_url(self, recipient_id, attachment_type, attachment_url,
                             notification_type=NotificationType.regular):
