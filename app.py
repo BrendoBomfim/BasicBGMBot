5a931b6cd3319e18d33a0503bfd06b45ea0421fe
Brendo Bomfim <brendobomfim@sciosolutions.com.br>
2018-11-29 16:30:38 -0300

    opening for external connections

--- app.py
+++ app.py
@@ -124,4 +124,4 @@ def download_file(url):
 	return base64.b64encode(requests.get(url).content)
 
 if __name__ == "__main__":
-	app.run(ssl_context=('cert/cert.pem', 'cert/privkey.pem'))
+	app.run(host='0.0.0.0', ssl_context=('cert/cert.pem', 'cert/privkey.pem'))
